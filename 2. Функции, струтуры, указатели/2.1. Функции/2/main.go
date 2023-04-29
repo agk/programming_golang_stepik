f5cb512126e336b2544509996ec5d0afd443c4aa
Kucheryavenko Alexey <kucheryavenko.alexey@gmail.com>
2023-04-29 14:59:03 +0300

    --

---
+++ 2. Функции, струтуры, указатели/2.1. Функции/2/main.go
@@ -0,0 +1,36 @@
+/*
+Напишите функцию, находящую наименьшее из четырех введённых в этой же функции чисел.
+
+Входные данные
+Вводится четыре числа.
+
+Выходные данные
+Необходимо вернуть из функции наименьшее из 4-х данных чисел
+
+Sample Input:
+4 5 6 7
+
+Sample Output:
+4
+*/
+package new
+
+import "fmt"
+
+func minimumFromFour() int {
+	//print your code here
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+	return min(min(a, b), min(c, d))
+}
+
+func min(a int, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func main() {
+	fmt.Println(minimumFromFour())
+}
