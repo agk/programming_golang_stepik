b24d60fc929b84b4710f7f5cb840195f59936d3f
Kucheryavenko Alexey <kucheryavenko.alexey@gmail.com>
2025-11-14 16:49:58 +0300

    +maps

---
+++ 3/3.1/1/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// 1 var s []map[int]int
+	// 0 var users []string
+	// 0 var s []map[int]string{}
+	// 1 s := make(map[float32]int)
+	// 1 var s = []map[int]string{}
+	// 0 s := []map[int]int[uint18]
+
+	// fmt.Println(s)
+
+}
